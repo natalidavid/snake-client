fe0fccf19fc91b54feaffa8ab66d4e5f35558153
Natalie Davidovic <natalija.david@hotmail.com>
2021-01-14 19:51:50 +0000

    Snek

--- play.js
+++ play.js
@@ -2,3 +2,15 @@ const connect = require('./client.js');
 
 console.log('Connecting ...');
 connect();
+
+/**
+ * Setup User Interface 
+ * Specifically, so that we can handle user input via stdin
+ */
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  return stdin;
+}
