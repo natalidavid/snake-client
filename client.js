934ac47b027ee9b80bda2a7f8986bbcba2ab813f
Natalie Davidovic <natalija.david@hotmail.com>
2021-01-14 19:49:57 +0000

    Snek

--- client.js
+++ client.js
@@ -1,5 +1,9 @@
 const net = require('net');
 
+
+/**
+ * Establishes connection with the game server
+ */
 const connect = function() {
   const conn = net.createConnection({ 
     host: '135.23.222.131',
@@ -15,7 +19,6 @@ const connect = function() {
   conn.on('connect', () => {
     console.log('Connected to the server');
     conn.write('Name: SNK');
-
   });
   
   //event handler
@@ -23,6 +26,8 @@ const connect = function() {
   conn.on('data', (data) => {
     console.log('Server says: ', data);
   })
+
   return conn;
 }
+
 module.exports = connect;
