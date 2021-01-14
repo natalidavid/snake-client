e5c7f2e5fdff03fe3bd8bd2a71056137ff356faa
Natalie Davidovic <natalija.david@hotmail.com>
2021-01-14 19:43:08 +0000

    Snek

--- client.js
+++ client.js
@@ -8,8 +8,18 @@ const connect = function() {
 
   // interpret incoming data as text
   conn.setEncoding('utf8'); 
+
+  // somehow get a response back from the server after we connect to it which notifies us we connected!
+  // once we recieve the response from the server notifying us, as the client we are connected, then we can...
+  // console log for the users convience they connected to the server then writing back out to the server our snakes name
+  conn.on('connect', () => {
+    console.log('Connected to the server');
+    conn.write('Name: SNK');
+
+  });
   
   //event handler
+  // do something when you receive Data
   conn.on('data', (data) => {
     console.log('Server says: ', data);
   })
