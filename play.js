19b88b4998b4dbddd1890eb8573c0de0ac897414
Natalie Davidovic <natalija.david@hotmail.com>
2021-01-14 20:25:18 +0000

    Snek

--- play.js
+++ play.js
@@ -7,10 +7,30 @@ connect();
  * Setup User Interface 
  * Specifically, so that we can handle user input via stdin
  */
+
 const setupInput = function() {
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
-  stdin.resume();
+
+  const handleUserInput = function(key) {
+    if (key === '\u0003') {
+      process.exit();
+    }
+  }
+
+  stdin.on('data', handleUserInput);
   return stdin;
 }
+
+const { connect } = require('./client');
+console.log('Connecting ...');
+connect(); 
+connect();
+
+/**
+ * Setup User Interface 
+ * Specifically, so that we can handle user input via stdin
+ */
+
+setupInput();
