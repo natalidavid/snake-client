f5677e1345cd461c17588823a0387499c3a4f90e
Natalie Davidovic <natalija.david@hotmail.com>
2021-01-15 02:51:11 +0000

    Stdin

--- client.js
+++ client.js
@@ -1,13 +1,13 @@
-const net = require('net');
 
+const net = require('net');
 
 /*
  * Establishes connection with the game server
  */
 const connect = function() {
   const conn = net.createConnection({ 
-    host: '135.23.222.131',
-    port: 50542
+    host: '138.197.169.51',
+    port: 50541
   });
 
   // interpret incoming data as text
--- play.js
+++ play.js
@@ -7,30 +7,16 @@ connect();
  * Setup User Interface 
  * Specifically, so that we can handle user input via stdin
  */
-
 const setupInput = function() {
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
-
-  const handleUserInput = function(key) {
+  stdin.handleUserInput = function(key) {
     if (key === '\u0003') {
       process.exit();
     }
   }
-
   stdin.on('data', handleUserInput);
+  
   return stdin;
 }
-
-const { connect } = require('./client');
-console.log('Connecting ...');
-connect(); 
-connect();
-
-/**
- * Setup User Interface 
- * Specifically, so that we can handle user input via stdin
- */
-
-setupInput();
