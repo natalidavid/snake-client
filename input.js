c2a4fffbdda4cfe80a8c8024619426cabf11b021
Natalie Davidovic <natalija.david@hotmail.com>
2021-01-15 03:13:05 +0000

    Input.js

--- input.js
+++ input.js
@@ -6,12 +6,13 @@ const setupInput = function() {
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
-  stdin.handleUserInput = function(key) {
+  const handleUserInput = function(key) {
     // for exiting with ctrl + c
     if (key === '\u0003') {
       process.exit();
     }
   }
+  //data from stdin & callback function
   stdin.on('data', handleUserInput);
   
   return stdin;
